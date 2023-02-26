425a724c1c0f3c8008c9c3bb0893ba864675e5e5
Frederik <frederik@jens-schwarz.com>
2023-02-26 19:04:41 +1300

    added public ip and arn output

--- ouptut.tf
+++ ouptut.tf
@@ -5,3 +5,19 @@ output "instance_id" {
   description = "Instance ID"
   value = var.create_aws_instance ? module.aws_instance[0].id : ""
 }
+
+output "instance_arn" {
+  depends_on = [
+    module.aws_instance[0]
+  ]
+  description = "Instance ARN"
+  value = var.create_aws_instance ? module.aws_instance[0].arn : ""
+}
+
+output "instance_public_ip" {
+  depends_on = [
+    module.aws_instance[0]
+  ]
+  description = "Instance public ip"
+  value = var.create_aws_instance ? module.aws_instance[0].public_ip : ""
+}
